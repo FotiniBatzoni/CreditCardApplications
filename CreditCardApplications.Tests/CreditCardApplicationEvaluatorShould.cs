9ef783209fee14977d5b27db7e92b01b98c88949
fotini <f.batzoni@gmail.com>
2023-01-16 17:38:12 +0200

    Adding Test AcceptingHighIncomeApplecations

---
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace CreditCardApplications.Tests
+{
+    public class CreditCardApplicationEvaluatorShould
+    {
+        [Fact]
+        public void AcceptingHighIncomeApplecations()
+        {
+            //sut = systemUnderTest
+            var sut = new CreditCardApplicationEvaluator();
+
+            var application = new CreditCardApplication { GrossAnnualIncome = 100_000 };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+    }
+}
