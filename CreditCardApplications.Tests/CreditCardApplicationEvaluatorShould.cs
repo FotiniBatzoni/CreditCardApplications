f5eb66f51e4d3f0a1133c3e9ae3709af345dcaeb
fotini <f.batzoni@gmail.com>
2023-01-17 17:51:33 +0200

    6 ways for argument matching to set up pock methods

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -45,16 +45,48 @@ namespace CreditCardApplications.Tests
         {
             Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
 
+
+            //1 way
             //to setup isValid to true
-            mockValidator.Setup(x => x.IsValid("x")).Returns(true);
-  
+            //it returns true only when    FrequentFlyerNumber = "x"
+
+            //1. mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+
+            //2 way
+            //it returns true when   FrequentFlyerNumber = is any string
+
+            //2. mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
+
+            //3 way
+            //it returns true when   FrequentFlyerNumber = is a number which starts with "y"
+
+            //3. mockValidator.Setup(x => x.IsValid(It.Is<string>(number => number.StartsWith("y")))).Returns(true);
+
+            //4 way
+            //it returns true when   FrequentFlyerNumber = is in Range from "a" to "z"
+            //Moq.Range.Inclusive means that "a" is included to the Range
+            //if Moq.Range.Exclusive means that "a" is not included to the Range
+
+            //4. mockValidator.Setup(x => x.IsValid(It.IsInRange<string>("a","z", Range.Inclusive))).Returns(true);
+
+            //5 way
+            //it returns true when   FrequentFlyerNumber = is "z" || "y" || "x"
+
+            //5. mockValidator.Setup(x => x.IsValid(It.IsIn("z", "y", "x"))).Returns(true);
+
+            //6 way 
+            //it returns true when   FrequentFlyerNumber = match Regex
+
+            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z]"))).Returns(true);
+
+
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
             var application = new CreditCardApplication
             {
                 GrossAnnualIncome = 19_999,
                 Age = 42,
-                FrequentFlyerNumber = "x"
+                FrequentFlyerNumber = "y"
             };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
