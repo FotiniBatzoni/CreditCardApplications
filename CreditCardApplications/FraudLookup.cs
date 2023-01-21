798cf3cb2b873814efd39569d5ec93f0127f74dd
fotini <f.batzoni@gmail.com>
2023-01-21 12:02:01 +0200

    Adding FraudLookup class

---
+++ CreditCardApplications/FraudLookup.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CreditCardApplications
+{
+    internal class FraudLookup
+    {
+        public bool isFraudRisk(CreditCardApplication application)
+        {
+            if( application.LastName == "Smith")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
